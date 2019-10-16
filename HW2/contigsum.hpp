945058a415941df7bc53b26e410c8856bc13e3eb
Ian Ferguson <imferguson@alaska.edu>
2019-10-16 15:10:05 -0800

    Add max functions. Start conquer, workhorse function that will do majority of the work.

--- HW2/contigsum.hpp
+++ HW2/contigsum.hpp
@@ -5,6 +5,25 @@
 #ifndef HW2_CONTIGSUM_HPP
 #define HW2_CONTIGSUM_HPP
 
+int max (int a, int b)
+{
+    if(a>b)
+    {
+        return a;
+    } else return b;
+}
+
+int max (int a, int b, int c)
+{
+    return max(max(a, b), c);
+}
+
+template<typename RAITER>
+int Conquer(RAITER first, RAITER mid,  RAITER last)
+{
+
+}
+
 template<typename RAITER>
 int contigSum(RAITER first, RAITER last){
     return 0;
