01d9ca547229d06d2b2cf9f51aed973c8cf7d445
Ian Ferguson <imferguson@alaska.edu>
2019-10-16 14:36:34 -0800

    first commit. HW set up.

---
+++ HW2/contigsum.hpp
@@ -0,0 +1,13 @@
+//
+// Created by Ian Ferguson on 2019-10-16.
+//
+
+#ifndef HW2_CONTIGSUM_HPP
+#define HW2_CONTIGSUM_HPP
+
+template<typename RAITER>
+int contigSum(RAITER first, RAITER last){
+    return 0;
+}
+
+#endif //HW2_CONTIGSUM_HPP
