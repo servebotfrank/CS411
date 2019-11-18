e1499c5114e011b5633567ae60f4b9efd194c1cb
Servebotfrank <servebotfrank@gmail.com>
2019-11-17 16:57:55 -0900

    First commit. Does not compile currently.

---
+++ HW5/build.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Servebotfrank on 11/17/2019.
+//
+
+#include "build.h"
---
+++ HW5/build.h
@@ -0,0 +1,8 @@
+//
+// Created by Servebotfrank on 11/17/2019.
+//
+
+#ifndef HW5_BUILD_H
+#define HW5_BUILD_H
+
+#endif //HW5_BUILD_H
